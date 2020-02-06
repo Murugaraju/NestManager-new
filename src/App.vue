f3e4b5074fd10ed10cb971c3929ba4d57ce9b247
Murugaraju <murugan.iiit2326@gmail.com>
2020-02-06 10:48:30 +0530

    added login component

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    
     <v-content>
       
-      <Dashboard/>
+      <Login/>
 
      
 
@@ -49,14 +13,15 @@
 
 <script>
   
-import Dashboard from './components/Dashboard/Dashboard';
-
+// import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login'
 export default {
   name: 'App',
 
   components: {
     
-    Dashboard
+    // Dashboard
+    Login
   },
 
   data: () => ({
---
+++ src/components/Login/Login.vue
@@ -0,0 +1,45 @@
+<template>
+ <v-card width='400' raised class='mx-auto mt-5'>
+    <v-card-title>Login to NestManager PG</v-card-title>
+    <v-card-text>
+        <v-form>
+            <v-text-field 
+                label='Username'
+                
+                prepend-icon="mdi-account-circle"
+            
+            />
+            <v-text-field 
+            label='Password'
+            
+            prepend-icon="mdi-lock"
+            :type="showPassword?'text':'password'"
+            :append-icon="showPassword?'mdi-eye':'mdi-eye-off'"
+            @click:append="showPassword=!showPassword"
+        
+            
+            />
+
+        </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer/>
+        <v-btn color="info">Login</v-btn>
+
+        
+    </v-card-actions>
+
+</v-card>
+</template>
+<script>
+export default {
+    name:'Login',
+    data:()=>{
+        return{
+            showPassword:false,
+        }
+    }
+}
+</script>
