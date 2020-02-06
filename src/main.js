9fae11ae22c65a29f76a9af04b5984dccface3c4
Murugaraju <murugan.iiit2326@gmail.com>
2020-02-07 00:15:41 +0530

    removed and added django latest 3.x, for asyn related features

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import {router} from './router';
-import {VueRouter} from 'vue-router';
+
 
 Vue.config.productionTip = false
-Vue.use(VueRouter)
+
 new Vue({
   vuetify,
   router,
